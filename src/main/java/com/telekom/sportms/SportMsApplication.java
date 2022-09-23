e57d4ccc91db04db585091db6256d33c36634b8f
Kristóf Bodroghy <bodroghykristof@gmail.com>
2022-09-23 17:07:06 +0200

    create project

---
+++ src/main/java/com/telekom/sportms/SportMsApplication.java
@@ -0,0 +1,13 @@
+package com.telekom.sportms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportMsApplication.class, args);
+	}
+
+}
